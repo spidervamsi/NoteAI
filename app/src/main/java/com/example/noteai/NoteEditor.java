783be80cf67c0eeec5a71ee04dcbf7fd23f45e75
spidervamsi <spidervamsi@gmail.com>
2020-04-11 21:09:09 -0400

    cursor moved to end of the text

--- app/src/main/java/com/example/noteai/NoteEditor.java
+++ app/src/main/java/com/example/noteai/NoteEditor.java
@@ -17,12 +17,14 @@ public class NoteEditor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
         text = findViewById(R.id.completeText);
+        text.setSelection(text.getText().length());
         loadData();
     }
 
     private void loadData(){
         if(getIntent().hasExtra("Text")){
             text.setText(getIntent().getStringExtra("Text"));
+            text.setSelection(text.getText().length());
         }
         if(getIntent().hasExtra("Pos")){
             pos = getIntent().getIntExtra("Pos",-1);
