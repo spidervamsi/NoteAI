03c58fb513532fddc7ce7993a2aa07f945180f98
spidervamsi <spidervamsi@gmail.com>
2020-07-02 16:46:28 -0400

    selection properly added

--- app/src/main/java/com/example/noteai/MyAdapter.java
+++ app/src/main/java/com/example/noteai/MyAdapter.java
@@ -3,6 +3,11 @@ package com.example.noteai;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +18,7 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,17 +32,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     ArrayList<Long> selectedValues;
     ViewGroup vg;
     MenuItem mainDeleteButton;
+    Boolean delMode = false;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView text;
         ConstraintLayout mainLayout;
-        CheckBox checkBox;
 
         public MyViewHolder(@NonNull View itemView){
             super(itemView);
             text = itemView.findViewById(R.id.textId);
             mainLayout = itemView.findViewById(R.id.mainLayout);
-            checkBox  = itemView.findViewById(R.id.checkDel);
             selectedValues = new ArrayList<Long>();
 
         }
@@ -72,21 +77,25 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, NoteEditor.class);
-                intent.putExtra("Text",text.get(position).getBody());
-                intent.putExtra("RowId",text.get(position).getRowId());
-                context.startActivity(intent);
-
+                if(delMode){
+                    ConstraintLayout cl = (ConstraintLayout) view;
+                    updateSelectedItems(position,cl);
+                }else{
+                    Intent intent = new Intent(context, NoteEditor.class);
+                    intent.putExtra("Text",text.get(position).getBody());
+                    intent.putExtra("RowId",text.get(position).getRowId());
+                    context.startActivity(intent);
+                }
             }
         });
-
 
         holder.mainLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
                 Log.i("notedev","on long click");
                 try{
-                    mainDeleteButton.setVisible(true);
+                    ConstraintLayout cl = (ConstraintLayout) view;
+                    updateSelectedItems(position,cl);
                 }catch (Exception e){
                     Log.i("notedev","visibility exception "+e.getMessage());
                 }
@@ -95,25 +104,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         });
 
 
-        holder.checkBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Log.i("notedev", "checkbox " + Integer.toString(position));
-                    CheckBox c = (CheckBox) view;
-                    if(c.isChecked()){
-                        selectedValues.add(text.get(position).getRowId());
-                    }else {
-                        selectedValues.remove(text.get(position).getRowId());
-                    }
+    }
 
-                }catch (Exception e){
-                    Log.i("notedev","check exception "+e.getMessage());
-                }
-
+    public void updateSelectedItems(int position,ConstraintLayout cl){
+        try{
+            long id = text.get(position).getRowId();
+            if(selectedValues.contains(id)){
+//              TODO Need to set the background color properly
+                cl.getViewById(R.id.rowCard).setBackgroundColor(0x00000000);
+                selectedValues.remove(id);
+            }else{
+                cl.getViewById(R.id.rowCard).setBackgroundColor(Color.rgb(190,241,222));
+                selectedValues.add(id);
             }
-        });
 
+            if(selectedValues.isEmpty()){
+                delMode = false;
+            }else {
+                delMode = true;
+            }
+            mainDeleteButton.setVisible(delMode);
+        }catch (Exception e){
+            Log.i("notedev","visibility exception "+e.getMessage());
+        }
     }
 
     @Override
